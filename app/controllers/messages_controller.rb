c12096dd1b29df560349c801acff5ddfbbb73b74
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-10 18:16:51 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,13 +2,16 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new 
+    @messages = @room.messages.includes(:user) #@room.messagesで、このチャットルームに紐づいているmessagesを取得する
   end
 
   def create
-    @room = Room.find(params[:room_id])
+    @room = Room.find(params[:room_id]) #引数＠roomには、「１」「２」「３」・・・等、roomのIDが入る
     @message = @room.messages.new(message_params)
     if @message.save
-        redirect_to room_messages_path(@room) #なぜredirect_to action: :indexではないのか？→引数＠roomを渡すため。URLは、rooms/@room.id/messagesとなるので、引数が必要。      else
+        redirect_to room_messages_path(@room) #なぜredirect_to action: :indexではないのか？→引数＠roomを渡すため。URLは、rooms/@room.id/messagesとなるので、引数が必要。
+        else
+        @messages = @room.messages.includes(:user)  #投稿に失敗した@messageの情報を保持しつつindex.html.erbを参照することがでる（この時、indexアクションは経由ない）
         render :index
   end
 
@@ -17,4 +20,5 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
+end
 end
